857ee62eab1fca6c05143e8be0aa9304e5fc1503
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-24 15:55:16 -0300

    near the end

--- js/script.js
+++ js/script.js
@@ -4,6 +4,50 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky")
         }else{
             $('.navbar').removeClass("sticky")
+        }if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
     });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    var typed = new Typed(".typing", {
+        strings:["Devoloper", "Trainee", "Technology Lover"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings:["About Me", "Sobre Mim"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    var typed = new Typed(".typing-3", {
+        strings:["Experiences", "Experiências"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    var typed = new Typed(".typing-4", {
+        strings:["My Skills", "Minhas Habilidades"],
+        typeSpeed:100,
+        backSpeed:50,
+        loop:true
+    });
+
+    var typed = new Typed(".typing-5", {
+        strings:["Contact", "Contato"],
+        typeSpeed:100,
+        backSpeed:50,
+        loop:true
+    });
+
 });
